9d5173384b6610df873aceb2d43ce257256296a9
Spudsdude <spudsdude@gmail.com>
2009-07-14 17:40:17 +0000

    another merge fix

--- miptvupdate/My Project/AssemblyInfo.vb
+++ miptvupdate/My Project/AssemblyInfo.vb
@@ -31,10 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<<<<<<< .mine
 <Assembly: AssemblyVersion("2.0.1.2904")> 
 <Assembly: AssemblyFileVersion("2.0.1.2904")> 
-=======
-<Assembly: AssemblyVersion("2.0.1.2903")> 
-<Assembly: AssemblyFileVersion("2.0.1.2903")> 
->>>>>>> .r159
